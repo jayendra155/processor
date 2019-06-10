27c166d77f9fb778979ddaab0fca950ff76fafd2
Jayendra Vikram Singh <jayendra.singh@cleartrip.com>
2019-06-11 02:14:20 +0530

    first commit

---
+++ src/main/java/net/samurai/katana/ProcessorApplication.java
@@ -0,0 +1,16 @@
+package net.samurai.katana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, MongoAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class ProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessorApplication.class, args);
+    }
+
+}
